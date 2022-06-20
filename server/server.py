00d1d50064ab399f25e15358a3f597ab0663cd90
itstechsupport <maciek.z.yt.pl@gmail.com>
2022-06-20 23:42:48 +0200

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,5 +1,8 @@
+from anyio import open_signal_receiver
 import pyautogui
 import socket
+from PIL import Image
+from io import BytesIO
 
 s = socket.socket()
 print ("Socket successfully created")
@@ -12,9 +15,26 @@ print ("Socket binded to %s" %(port))
 s.listen(5)
 print ("Socket is listening")
 
+
+screen = pyautogui.screenshot()
+screen.save('img.png')
+open_screen = open(screen, 'rb')
+bytes = bytearray(screen.read)
+
+stream = BytesIO(bytes)
+
+image = Image.open(stream).convert("RGBA")
+stream.close()
+image.show()
+
+
+
 c, addr = s.accept()
 while True:
-   
-   key = c.recv(1024).decode()
-   print(key)
-   pass
+    screen = open(pyautogui.screenshot(), 'rb')
+    bytes = bytearray(screen.read)
+    c.sendall(bytes)
+    key = c.recv(1024).decode()
+    #print(key)
+    pass
+
